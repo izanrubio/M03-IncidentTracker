5a28de55b7dec0b11784ab19e3709cd6cc0e50aa
Izan <irubio@montperdut.cat>
2026-02-02 20:39:46 +0100

    Versió final

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+# core/admin.py
+from django.contrib import admin
+from .models import SecurityIncident
+
+@admin.register(SecurityIncident)
+class SecurityIncidentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'severity', 'detected_at')
